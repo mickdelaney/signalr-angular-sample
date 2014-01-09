ee06d85caba6bfa111680cc2aca44c9d8e6fd2fc
mick delaney <mickdelaney@gmail.com>
2014-01-09 21:45:18 +0000

    first commit

---
+++ SignalR-Sample/BusHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace SignalR_Sample
+{
+    public class BusHub : Hub
+    {
+        public void Publish(string key, object data)
+        {
+            Clients.All.publish(key, data);
+        }
+    }
+}
